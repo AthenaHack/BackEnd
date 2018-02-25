eceeda4fe446f62ee5d087e161e7be75fdf546ae
Diane Tobit <dianetobit@gmail.com>
2018-02-25 12:47:24 -0800

    move request

--- index.js
+++ index.js
@@ -15,20 +15,21 @@ const MessagingResponse = require('twilio').twiml.MessagingResponse;
 
 app.get('/', function (req, res) {
   res.send('Hello World!')
+  request('https://api.mlab.com/api/1/databases/athenahacks/collections/students?apiKey=o2gZ1lynVLWp2xf46oNSrA0avHlH5rUI', { json: true }, (err, res, main) => {
+    if (err) { return console.log(err); }
+    console.log(main.length);
+    for(var i = 0; i < main.length; i++){
+      client.messages.create({
+          body: "This is an alert. Stay away from the danger!!",
+          to: main[i]["phoneNumber"],  // Text this number
+          from: process.env.SENDERNUM // From a valid Twilio number
+      })
+      .then((message) => console.log(message.sid));
+  }
+  });
 })
 
-request('https://api.mlab.com/api/1/databases/athenahacks/collections/students?apiKey=o2gZ1lynVLWp2xf46oNSrA0avHlH5rUI', { json: true }, (err, res, main) => {
-  if (err) { return console.log(err); }
-  console.log(main.length);
-  for(var i = 0; i < main.length; i++){
-    client.messages.create({
-        body: "This is an alert. Stay away from the danger!!",
-        to: main[i]["phoneNumber"],  // Text this number
-        from: process.env.SENDERNUM // From a valid Twilio number
-    })
-    .then((message) => console.log(message.sid));
-}
-});
+
 
 app.listen(3000, function () {
   console.log('App listening on port 3000!')
