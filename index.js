95ed54e257cf1dfc5315efe7553282b19f52aa5b
Diane Tobit <dianetobit@gmail.com>
2018-02-25 04:11:52 -0800

    send multiple alerts thru twilio

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ var express = require('express');
 var app = express();
 var fs = require('fs');
 var twilio = require('twilio');
+var db = require('./database');
+var request = require('request');
 
 var accountSid = process.env.ACCOUNTSID; // Your Account SID from www.twilio.com/console
 var authToken = process.env.AUTHTOKEN;   // Your Auth Token from www.twilio.com/console
@@ -15,12 +17,18 @@ app.get('/', function (req, res) {
   res.send('Hello World!')
 })
 
-client.messages.create({
-    body: "This is an alert. Stay away from the danger!!",
-    to: '+19253395106',  // Text this number
-    from: process.env.SENDERNUM // From a valid Twilio number
-})
-.then((message) => console.log(message.sid));
+request('https://api.mlab.com/api/1/databases/athenahacks/collections/students?apiKey=o2gZ1lynVLWp2xf46oNSrA0avHlH5rUI', { json: true }, (err, res, main) => {
+  if (err) { return console.log(err); }
+  console.log(main.length);
+  for(var i = 0; i < main.length; i++){
+    client.messages.create({
+        body: "This is an alert. Stay away from the danger!!",
+        to: main[i]["phoneNumber"],  // Text this number
+        from: process.env.SENDERNUM // From a valid Twilio number
+    })
+    .then((message) => console.log(message.sid));
+}
+});
 
 app.listen(3000, function () {
   console.log('App listening on port 3000!')
